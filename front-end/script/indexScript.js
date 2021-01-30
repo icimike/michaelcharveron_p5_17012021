f4eda6fbcd21eeb2867f749d69ff71b563a5528b
Michael <icimike@hotmail.com>
2021-01-30 18:30:22 +0100

    one small step

--- front-end/script/indexScript.js
+++ front-end/script/indexScript.js
@@ -1,55 +1,92 @@
-const productTeddy = document.getElementById('teddies');
-let apiTeddy;
+main()
 
-const fetchTeddy = async() => {
-  apiTeddy = await fetch (
-    'http://localhost:3000/api/teddies').then(res => res.json());
+async function main() {
+  const products = await getProducts()
+  for (product of products) {
+    displayProduct(product)
+  }
 }
 
-const showTeddies = async() => {
-  await fetchTeddy();
+function getProducts() {
+  return fetch("http://localhost:3000/api/teddies")
+    .then(function(httpBodyResponse) {
+      return httpBodyResponse.json()
+    })
+    .then(function(products) {
+      return products
+    })
+    .catch(function(error) {
+      alert(error)
+    })
+}
 
-  productTeddy.innerHTML = (
-    apiTeddy
-    .map(teddy =>(
-      `
-    <li>
-      <div class="card mb-3">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img class="img-thumbnail shadow" src="${teddy.imageUrl}" />
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">${teddy.name}</h5>
-              <p class="card-text">${teddy.description}</p>
-              <p class="card-text text-muted fs-5">
-                ${numberWithCommas(teddy.price)}€
-              </p>
-             <br />
-             <a
-             id="btn-product"
-             class="btn btn-outline-primary w-25"
-             href="/front-end/product.html"
-             role="button"
-             >Détail</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </li>
-      `
-    )).join('')
-  );
-};
+function displayProduct() {
+  const templateElt = document.getElementById("templateProduct")
+  const cloneElt = document.importNode(templateElt.content, true)
 
-showTeddies();
+  cloneElt.getElementById("imgTeddy").src = product.imageUrl
+  cloneElt.getElementById("titleTeddy").textContent = product.name
+  cloneElt.getElementById("textTeddy").textContent = product.description
+  cloneElt.getElementById("priceTeddy").textContent = `${product.price / 100}.00 €`
+  cloneElt.getElementById("btn-product").href = `product.html?id=${product._id}`
+
+  document.getElementById("cardProduct").appendChild(cloneElt)
+}
+
+
+
+// const productTeddy = document.getElementById('teddies');
+// let apiTeddy;
+
+// const fetchTeddy = async() => {
+//   apiTeddy = await fetch (
+//     'http://localhost:3000/api/teddies').then(res => res.json());
+// }
+
+// const showTeddies = async() => {
+//   await fetchTeddy();
+
+//   productTeddy.innerHTML = (
+//     apiTeddy
+//     .map(teddy =>(
+//       `
+//     <li>
+//       <div class="card mb-3">
+//         <div class="row g-0">
+//           <div class="col-md-4">
+//             <img class="img-thumbnail shadow" src="${teddy.imageUrl}" />
+//           </div>
+//           <div class="col-md-8">
+//             <div class="card-body">
+//               <h5 class="card-title">${teddy.name}</h5>
+//               <p class="card-text">${teddy.description}</p>
+//               <p class="card-text text-muted fs-5">
+//                 ${numberWithCommas(teddy.price)}€
+//               </p>
+//              <br />
+//              <a
+//              id="btn-product"
+//              class="btn btn-outline-primary w-25"
+//              href=
+//              role="button"
+//              >Détail</a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </li>
+//       `
+//     )).join('')
+//   );
+// };
+
+// showTeddies();
 
 
 // Fonction pour formater le prix //
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{2})+(?!\d))/g, ",");
-}
+// function numberWithCommas(x) {
+//   return x.toString().replace(/\B(?=(\d{2})+(?!\d))/g, ",");
+// }
 
 
 
--- front-end/script/productScript.js
+++ front-end/script/productScript.js
@@ -1,3 +1,66 @@
+// let apiUrl = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+//   ? "http://localhost:3000"
+//   : "http://localhost:3000/api/teddies"
+
+
+
+
+// Main function, auto executed at load time
+(async () => {
+    const productId = getProductId()
+    const productData = await getProductData(productId)
+    hydratePage(productData)
+  })()
+  
+  function getProductId() {
+    return new URL(window.location.href).searchParams.get('id')
+  }
+  
+  function getProductData(productId) {
+    return fetch(`http://localhost:3000/api/teddies/${productId}`)
+      .catch(error => { console.log(error) })
+      .then(httpBodyResponse => httpBodyResponse.json())
+      .then(productData => productData)
+  }
+  
+  function hydratePage(product) {
+    // Hydrate page with data
+    document.getElementById('imgTeddy').src = product.imageUrl
+    document.getElementById('titleTeddy').textContent = product.name
+    document.getElementById('priceTeddy').textContent = `${product.price / 100}.00 €`
+    document.getElementById('textTeddy').textContent = product.description
+    // document.getElementById('productColors').style.gridTemplateColumns = `repeat(${product.colors.length}, 1fr)`
+  
+    // Add event listeners on button
+    // document.getElementById('addToCart').onclick = (event) => {
+    //   event.preventDefault()
+    //   Cart.addProduct(product)
+    //   redirectToShoppingCart(product.name)
+    // }
+  
+    // Get parent element
+    // const colorsElt = document.getElementById('productColors')
+  
+    // Display all colors
+    // product.colors.forEach(color => {
+      // Get & clone template for one color
+    //   const templateElt = document.getElementById('productColor')
+    //   const cloneElt = document.importNode(templateElt.content, true)
+  
+      // Hydrate color clone
+    //   cloneElt.querySelector('div').style.backgroundColor = color
+  
+      // Display a new color
+    //   colorsElt.appendChild(cloneElt)
+    // })
+  }
+  
+//   function redirectToShoppingCart(productName) {
+//     window.location.href = `${window.location.origin}/cart.html?lastAddedProductName=${productName}`
+//   }
+
+
+
 // inserer un ARRAY dans liste déroulante :
 let selectColor = document.getElementById("selectColor"); 
 let colors = ["Tan", "Chocolate", "Black", "White"];
